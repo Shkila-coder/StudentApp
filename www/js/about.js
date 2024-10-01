cc84cd616876fd936268ce6a42aa31c03434b1c2
Шкила-кодер <n.suvorov99@mail.ru>
2024-10-02 01:23:44 +0300

    feat: created a pop-up window which shows info about app

---
+++ www/js/about.js
@@ -0,0 +1,23 @@
+// Открытие модального окна About
+function openAboutModal() {
+    document.getElementById("aboutModal").style.display = "block";
+}
+
+// Закрытие модального окна About
+function closeAboutModal() {
+    document.getElementById("aboutModal").style.display = "none";
+}
+
+// Функции для обработки нажатий на кнопки социальных сетей
+function openGitHub() {
+    window.open("https://github.com/Shkila-coder", "_blank");
+}
+
+function openTelegram() {
+    window.open("https://t.me/NikolasShkil", "_blank");
+}
+
+function openDiscord() {
+    window.open("https://discord.com/users/478648844385517578", "_blank");
+}
+
