e3c632aca335e3f0cb73bcee7f5be895e713c32b
Шкила-кодер <n.suvorov99@mail.ru>
2024-10-06 17:02:56 +0300

    fix: home screen loading

--- www/js/utils.js
+++ www/js/utils.js
@@ -1,4 +1,11 @@
+document.addEventListener('DOMContentLoaded', onDOMLoaded);
 
+function onDOMLoaded() {
+    document.getElementById('main-content').style.display = 'block';
+    document.getElementById('settings').style.display = 'none';
+    document.getElementById('profile').style.display = 'none';
+    updateTitle('Home'); // Обновляем заголовок
+}
 // Функция для обновления заголовка в синей полоске с использованием стрелки
 function updateTitle(subtitle) {
     const titleElement = document.querySelector('.title');
